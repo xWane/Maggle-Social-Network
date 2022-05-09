2f8ae4d5a5289914b1c6c488bdc7aaff49f6969f
clement <clementpnc@gmail.com>
2022-05-09 14:30:52 +0200

    test

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+$engine = "mysql";
+$host = "localhost";
+$port = 3306;
+$dbname = "projet";
+$username = "root";
+$password = "root";
+$pdo = new PDO("$engine:host=$host:$port;dbname=$dbname", $username, $password);
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -8,6 +18,29 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+
+<form method="get">
+    Nom :     <input type="text" name="nom" />
+    Age :     <input type="text" name="age" />
+    Adresse : <input type="text" name="adresse" />
+    <input type="submit" name="submit" /> 
+</form>
+
+<?php
+   // Vérifier si le formulaire est soumis 
+   if ( isset( $_GET['submit'] ) ) {
+     /* récupérer les données du formulaire en utilisant 
+        la valeur des attributs name comme clé 
+       */
+     $nom = $_GET['nom']; 
+     $age = $_GET['age']; 
+     $adresse = $_GET['adresse'];
+     // afficher le résultat
+     echo '<h3>Informations récupérées en utilisant GET</h3>'; 
+     echo 'Nom : ' . $nom . ' Age : ' . $age . ' Adresse : ' . $adresse; 
+     exit;
+  }
+?>
     
 </body>
 </html>
