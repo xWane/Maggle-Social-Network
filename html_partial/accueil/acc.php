39fcd36ca1ab77138df9b7c1b71d4e10c7afa6dd
clement <clementpnc@gmail.com>
2022-05-16 11:21:32 +0200

    add accueil

---
+++ html_partial/accueil/acc.php
@@ -0,0 +1,12 @@
+<?php require '../head.php'; ?>
+<?php require '../left.php'; ?>
+<?php require 'center-acc.php'; ?>
+<?php require 'right-acc.php'; ?>
+<?php require '../footer.php'; ?>
+
+
+
+    
+
+    
+
---
+++ html_partial/accueil/right-acc.php
@@ -0,0 +1,94 @@
+<!-- SECTION : Right Container -->
+<main class="container-right">
+
+<div class="sticky">
+
+    <div class="research align">
+
+        <a href="" class="align"> <img src="../../public/icon/search.svg" alt="Rechercher" class="little-icon"> </a>
+        <input type="search" name="" class="search" placeholder="Recherche">
+
+    </div>
+
+    <div class="block">
+
+        <span class="title-block">Tendences Maggle</span>
+
+        <div class="item-block">
+            <div class="trend align-end">
+                <div class="text-trend">
+                    <span class="name-trend">#ONEPIECE1049</span>
+                    <span class="publication-trend">60 000 publications</span>
+                </div>
+                <a href="" class="align"> <img src="../../public/icon/more-horiz.svg" alt="Rechercher" class="little-icon"> </a>
+            </div>
+
+            <div class="trend align-end">
+                <div class="text-trend">
+                    <span class="name-trend">#ONEPIECE1049</span>
+                    <span class="publication-trend">60 000 publications</span>
+                </div>
+                <a href="" class="align"> <img src="../../public/icon/more-horiz.svg" alt="Rechercher" class="little-icon"> </a>
+            </div>
+
+            <div class="trend align-end">
+                <div class="text-trend">
+                    <span class="name-trend">#ONEPIECE1049</span>
+                    <span class="publication-trend">60 000 publications</span>
+                </div>
+                <a href="" class="align"> <img src="../../public/icon/more-horiz.svg" alt="Rechercher" class="little-icon"> </a>
+            </div>
+
+            <div class="trend align-end">
+                <div class="text-trend">
+                    <span class="name-trend">#ONEPIECE1049</span>
+                    <span class="publication-trend">60 000 publications</span>
+                </div>
+                <a href="" class="align"> <img src="../../public/icon/more-horiz.svg" alt="Rechercher" class="little-icon"> </a>
+            </div>
+
+            <div class="trend align-end">
+                <div class="text-trend">
+                    <span class="name-trend">#ONEPIECE1049</span>
+                    <span class="publication-trend">60 000 publications</span>
+                </div>
+                <a href="" class="align"> <img src="../../public/icon/more-horiz.svg" alt="Rechercher" class="little-icon"> </a>
+            </div>
+
+            <span class="more">Voir plus</span>
+        </div>
+    </div>
+
+    <div class="block">
+
+        <span class="title-block">Favoris</span>
+
+        <div class="item-block">
+            <div class="fovoris">
+                <a href="" class=" align"> <img src="../../public/img/pp.jpg" alt="Profile" class="icon profile-picture"> <span class="text-fovoris">Fabien</span> </a>
+            </div>
+            <div class="fovoris">
+                <a href="" class=" align"> <img src="../../public/img/pp.jpg" alt="Profile" class="icon profile-picture"> <span class="text-fovoris">Martin</span> </a>
+            </div>
+            <div class="fovoris">
+                <a href="" class=" align"> <img src="../../public/img/pp.jpg" alt="Profile" class="icon profile-picture"> <span class="text-fovoris">Ewan</span> </a>
+            </div>
+            <div class="fovoris">
+                <a href="" class=" align"> <img src="../../public/img/pp.jpg" alt="Profile" class="icon profile-picture"> <span class="text-fovoris">Vitomir</span> </a>
+            </div>
+            <div class="fovoris">
+                <a href="" class=" align"> <img src="../../public/img/pp.jpg" alt="Profile" class="icon profile-picture"> <span class="text-fovoris">Clément</span> </a>
+            </div>
+            <div class="fovoris">
+                <a href="" class=" align"> <img src="../../public/img/pp.jpg" alt="Profile" class="icon profile-picture"> <span class="text-fovoris">Steven</span> </a>
+            </div>
+            <div class="fovoris">
+                <a href="" class=" align"> <img src="../../public/img/pp.jpg" alt="Profile" class="icon profile-picture"> <span class="text-fovoris">Anthony</span> </a>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+</main>
