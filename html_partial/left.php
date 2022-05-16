37ec5508fb77b1a9fd5df4b79bc6d507adbf559b
clement <clementpnc@gmail.com>
2022-05-16 14:15:48 +0200

    chage groupe

--- html_partial/left.php
+++ html_partial/left.php
@@ -34,18 +34,18 @@
 <div class="sticky">
 
     <!-- Logo -->
-    <a href="index.php"><h1 class="title">maggle</h1></a>
+    <a href="../html_partial/accueil/accueil.php"><h1 class="title">maggle</h1></a>
 
     <!-- Item list -->
     <div class="nav-bar">
         <div class="item">
-            <a href="index.php" class="color1 align"> <img src="../../public/icon/home.svg" alt="Accueil" class="icon"> <span class="text-nav-bar">Accueil</span> </a>
+            <a href="../html_partial/accueil/accueil.php" class="color1 align"> <img src="../../public/icon/home.svg" alt="Accueil" class="icon"> <span class="text-nav-bar">Accueil</span> </a>
         </div>
         <div class="item">
             <a href="" class="color2 align"> <img src="../../public/img/pp.jpg" alt="Profile" class="icon profile-picture"> <span class="text-nav-bar">Profil</span> </a>
         </div>
         <div class="item">
-            <a href="groupe.php" class="color3 align"> <img src="../../public/icon/groupes.svg" alt="Groupes" class="icon"> <span class="text-nav-bar">Groupes</span> </a>
+            <a href="../html_partial/groupe/groupe.php" class="color3 align"> <img src="../../public/icon/groupes.svg" alt="Groupes" class="icon"> <span class="text-nav-bar">Groupes</span> </a>
         </div>
         <div class="item">
             <a href="" class="color4 align"> <img src="../../public/icon/chat.svg" alt="Messages" class="icon"> <span class="text-nav-bar">Messages</span> </a>
