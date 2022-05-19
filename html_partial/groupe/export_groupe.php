f3ebe9ae641c84d4ee369a30a60a04de1abc6bcc
clement <clementpnc@gmail.com>
2022-05-19 11:44:45 +0200

    push

--- html_partial/groupe/export_groupe.php
+++ html_partial/groupe/export_groupe.php
@@ -2,17 +2,22 @@
 
 session_start() ;
 
-require '../../database/pdo.php';
+require '../../database/create_db.php';
 
 if(!empty($_POST['name_groupe'])) {
 
 $name_g = htmlspecialchars($_POST['name_groupe']);
 $name_g = strtolower($name_g);
+// On insère dans la base de données
+$insert = $pdo->prepare("INSERT INTO group (userMail, userPwd, userName, userSurname, profil_pic, profil_banner, visibility) VALUES (:email, :pass, :name, :surname, :imgProfil, :bannerProfil, 1)");
+$insert->execute(array(
+    ':email' => $email,
+    ':pass' => $pass,
+    ':name' => $name,
+    ':surname' => $surname,
+    ':imgProfil' => "pp.png",
+    ':bannerProfil' => "banner.jpg"
+));
+header('location: ./index.php');
 
-$add = $pdo->prepare('UPDATE user SET userName = :name WHERE user_id = :id');
-    $add->execute(array(
-    ':name' => $name_mod,
-    ':id' => $userId
-    ));
-}
 ?>
