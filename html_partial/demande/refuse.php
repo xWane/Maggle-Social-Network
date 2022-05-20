91467dd02d36402104eeeff45693e30d2de00ec1
clement <clementpnc@gmail.com>
2022-05-20 11:36:14 +0200

    fixed bug

--- html_partial/demande/refuse.php
+++ html_partial/demande/refuse.php
@@ -5,10 +5,9 @@ require '../left.php';
 $pose = $url;
 $pose = explode("err=",$pose);
 
-            $add = $pdo->prepare('DELETE FROM friends WHERE id = :id');
-            $add->execute(array(
-            ':id' => $pose[1]
-            ));
+            $add = $pdo->prepare('DELETE FROM friends WHERE user_id = :id AND friend_id = :idi OR user_id = :idi AND friend_id = :id');
+            $add->execute(array(':id' => $userId,
+                                ':idi' => $pose[1]));
 
             header('location: demande.php');
 
