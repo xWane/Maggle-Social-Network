77073eea26eaef140452a7101190fed254c56c0f
clement <clementpnc@gmail.com>
2022-05-20 02:28:23 +0200

    bug message

--- html_partial/messagerie/messagerie.php
+++ html_partial/messagerie/messagerie.php
@@ -6,18 +6,36 @@
 
 <?php
 
+
+//  $idi = $url;
+//  $idi = explode(".php",$idi);
+
+//  if($idi[1] !== ""){
+//     $idi = $url;
+//     $idi = explode("err=",$idi);
+//     $id2 = explode("_",$id[1]);
+//     echo "<span class='$id2[0]' id='none'>y</span>";
+//     echo "<span class='$id2[1]' id='none'>f</span>";
+//  }
+
+    $pose = $url;
+    $pose = explode(".err=",$pose);
+
+
 if($_SERVER["REQUEST_METHOD"] === "POST") {
     $text = $_POST["text"];
 
-    $query = $pdo->prepare("INSERT INTO `message` (text) VALUES (:text)");
+    $query = $pdo->prepare("INSERT INTO `message` (userId, userIdAmi, text) VALUES (:userid, :idconv, :text)");
     $query->execute([
+        ":userid" => $userId,
+        "idconv" => $pose[1],
         ":text" => $text
     ]);
 }
 ?>
 
 <!-- SECTION : Center Container -->
-<main class="container-center">
+<main class="container-center dis">
     <div class="user-messages">
 
     <?php
@@ -34,18 +52,40 @@ if($_SERVER["REQUEST_METHOD"] === "POST") {
         $user_visibilis = $datas['visibility'];
         if ($user_visibilis == 1) {
         echo    "<div class='favoris mess'>
-                <a href='messagerie.php?reg_err=$user_ids$userId$user_ids$userId' class=' align'> <img src='../../public/img/$user_pics' alt='Profile' class='icon profile-picture img-vide'>$user_names $user_surnames<span class='text-fovoris'></span> </a>
+                <a href='messagerie.php?reg_err=$userId'_'$user_ids class=' align'> <img src='../../public/img/$user_pics' alt='Profile' class='icon profile-picture img-vide'>$user_names $user_surnames<span class='text-fovoris'></span> </a>
                 </div>";
         }
     }
     ?>
+    </div>
 
     <div class="messages-messages">
+        <div class="top-mess">
+        <section id="theSection" class=""></section>
+            <div class="container-message y">
+                <div class="message you">
+                    <div class="textes">frgthjngbfghtjgthrgfghgfrhtgfht</div>
+                </div>
+                <img src='../../public/img/pp.jpg' alt='Profile' class='img-mess profile-picture img-vide'>
+            </div>
+            <div class="container-message i">
+                <div class="message id">
+                <div class="textes">frgthjtrjythgftryrjtht</div>
+                </div>
+                <img src='../../public/img/pp.jpg' alt='Profile' class='img-mess profile-picture img-vide'>
+            </div>
+        </div>
+
+        <div class="bot-mess align">
+        <form id="new_message_form" class="align-end" method="POST">
+        <textarea name="text" id="textArea" cols="30" rows="1" required></textarea>
+        <button>Envoyer</button>
+        </form >
+        </div>
 
     </div>
-<!-- <section id="theSection" class="modif-profil g4"></section>
 
-
+<!-- 
 <form id="new_message_form"  method="POST">
     <textarea name="text" id="textArea" cols="30" rows="10" required style=" padding: 5px;"></textarea>
     <button>Envoyer</button>
